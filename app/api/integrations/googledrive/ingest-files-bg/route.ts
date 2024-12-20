ccf285483f6338207d1c94a498aa36594dc4588c
Jack Mu <jackmu@umich.edu>
2024-12-20 06:29:37 -0800

    new route for background digestion

---
+++ app/api/integrations/googledrive/ingest-files-bg/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getLatestDriveCredential } from "@/app/api/integrations/googledrive/oauth";
+import { tasks } from "@trigger.dev/sdk/v3";
+
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
+export async function POST(request: NextRequest) {
+	const response = await request.json();
+	try {
+		const driveCreds = await getLatestDriveCredential(response.email);
+		const handle = await tasks.trigger("Drive-File-Ingestion", {
+			email: response.email,
+			token: driveCreds[0].access_token
+		});
+
+		console.log(handle);
+
+		return NextResponse.json(
+			{ status: 200 },
+		);
+
+	} catch (error) {
+		console.error("[Google Drive Trigger.dev]", error);
+		return NextResponse.json(
+			{ error: (error as Error).message },
+			{ status: 500 },
+		);
+	}
+}
